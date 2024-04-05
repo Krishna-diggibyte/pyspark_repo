7eebcc7ae813300ecb2c234c3a1ec2e4e7a28c88
Krishna-diggibyte <krishna.bhandari@diggibyte.com>
2024-04-06 02:42:02 +0530

    driver file of assignment 4 updated

--- src/assignment_4/driver.py
+++ src/assignment_4/driver.py
@@ -1,15 +1,58 @@
-from src.assignment_3.util import *
+from src.assignment_4.util import *
 
-spark = spark_session()
+spark = create_session()
 
-log_df = create_df(spark, log_data, log_schema)
-log_df.show()
+# 1. Read JSON file provided in the attachment using the dynamic function
+print("1. Read JSON file provided in the attachment using the dynamic function")
 
-log_df_updated = updateColumnName(log_df)
-log_df_updated.show()
+read_df=read_file(spark,path,schema)
+read_df.printSchema()
+read_df.show()
 
-actions_last_7 = action_performed_last_7(log_df_updated)
-actions_last_7.show()
+#2. flatten the data frame which is a custom schema
+print("2. flatten the data frame which is a custom schema")
 
-login_date_df = convert_timestamp_login_date(log_df_updated)
-login_date_df.show()
+flatted_df=flat_df(read_df)
+flatted_df.show()
+
+# 3. find out the record count when flattened and when it's not flattened(find out the difference why you are getting more count)
+print("3. find out the record count when flattened and when it's not flattened(find out the difference why you are getting more count)")
+print("Before Flatten: ", end="")
+print(read_df.count())
+print("\nAfter Flatten: ", end="")
+print(flatted_df.count())
+
+# 4. Differentiate the difference using explode, explode outer, posexplode functions
+print("\n4. Differentiate the difference using explode, explode outer, posexplode functions")
+print("explode")
+explode_display(read_df)
+print("explode_outer")
+explode_outer_display(read_df)
+print("pos explode")
+posexplode_display(read_df)
+print("pos explode outer")
+posexplode_outer_display(read_df)
+
+
+# 5. Filter the id which is equal to 0001
+print("5. Filter the id which is equal to 0001")
+check_id(flatted_df)
+
+# 6. convert the column names from camel case to snake case
+print("6. convert the column names from camel case to snake case")
+
+for column in flatted_df.columns:
+    flatted_df = flatted_df.withColumnRenamed(column,new=convert_lower(column))
+
+flatted_df.show()
+
+# 7. Add a new column named load_date with the current date
+print("7. Add a new column named load_date with the current date")
+
+flat_snake_date_df=add_current_date(flatted_df)
+flat_snake_date_df.show()
+
+# 8. create 3 new columns as year, month, and day from the load_date column
+print("8. create 3 new columns as year, month, and day from the load_date column")
+result_df=add_year_month(flat_snake_date_df)
+result_df.show()
