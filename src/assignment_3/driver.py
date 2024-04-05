d481e9fc7d8890409d48a9d0462d5abaa7caa5a9
Krishna-diggibyte <krishna.bhandari@diggibyte.com>
2024-04-06 02:41:54 +0530

    driver file of assignment 3 updated

--- src/assignment_3/driver.py
+++ src/assignment_3/driver.py
@@ -1,15 +1,25 @@
 from src.assignment_3.util import *
 
-spark = spark_session()
+spark = create_session()
 
-log_df = create_df(spark, log_data, log_schema)
-log_df.show()
+print("1. Create a Data Frame with custom schema creation by using Struct Type and Struct Field")
+my_dataframe=create_df(spark,data,schema)
+my_dataframe.show()
 
-log_df_updated = updateColumnName(log_df)
-log_df_updated.show()
+print("2.Column names should be log_id, user_id, user_activity, time_stamp using dynamic function")
 
-actions_last_7 = action_performed_last_7(log_df_updated)
-actions_last_7.show()
 
-login_date_df = convert_timestamp_login_date(log_df_updated)
-login_date_df.show()
+#renamed columns in the dataframe
+new_df = renaming_columns(my_dataframe, new_names)
+new_df.show()
+
+# 3. Write a query to calculate the number of actions performed by each user in the last 7 days
+print("3. Write a query to calculate the number of actions performed by each user in the last 7 days")
+actions_performed = action_count(new_df)
+actions_performed.show()
+
+#Convert the time stamp column to the login_date column with YYYY-MM-DD format with date type as its data type
+print("4. Convert the time stamp column to the login_date column with YYYY-MM-DD format with date type as its data type")
+result_df = update_column_login(new_df)
+result_df.printSchema()
+result_df.show()
